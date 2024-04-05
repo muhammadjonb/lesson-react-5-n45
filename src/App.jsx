609a6ba1df8205b4789ad8088e7a333dce748115
muhammadjonr <mrahmonaliev22032007@gmail.com>
2024-04-05 08:49:11 +0500

    teacher

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,72 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/Teachers";
+import Posts from "./pages/Posts";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import { AuthProvider, useAuth } from "./components/Auth";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import RequireAuth from "./components/RequireAuth";
+import Slider from "./components/Slider";
+import Teachers from "./pages/Teachers";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <AuthProvider>
+          <div className="flex_content">
+            <div>
+              <Slider />
+            </div>
+            <div style={{ width: "100%" }}>
+              <Header />
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <RequireAuth>
+                      <Home />
+                    </RequireAuth>
+                  }
+                />
+                <Route
+                  path="/students"
+                  element={
+                    <RequireAuth>
+                      <Posts />
+                    </RequireAuth>
+                  }
+                />
+                <Route
+                  path="/teachers"
+                  element={
+                    <RequireAuth>
+                      <Teachers />
+                    </RequireAuth>
+                  }
+                />
+                <Route path="/contact" element={<Contact />} />
+                <Route
+                  path="/profile"
+                  element={
+                    <RequireAuth>
+                      <Profile />
+                    </RequireAuth>
+                  }
+                />
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+          </div>
+        </AuthProvider>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
